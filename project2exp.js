e76fe26d485f79bb0ce57cc13d598ae394e9d548
palakk07 <90868437+palakk07@users.noreply.github.com>
2021-10-13 17:37:11 -0700

    Update project2exp.js

--- project2exp.js
+++ project2exp.js
@@ -1,25 +1,20 @@
 var screen = 1;
-var loginY = 650, signUpFade = 255, signUpFadeFlg = false;
-var loginRectFade = 255, loginRectFadeFlg = false; loginTxtFade = 255;
-var loginTxt = "Sign In", username, pwd;
-var bg;
+
+//1
+var bg1;
+
+function preload() {
+  bg1 = createImg('images/bg1.jpg');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  bg = color('blue');
-  textSize(40);
   textAlign(CENTER);
-  noStroke();
-    username = createInput();
-    username.position(60, 505);
-    username.size(width - 130, 35);
-  username.hide();
-  username.style('border-style', 'none');
-      pwd = createInput('', 'password');
-    pwd.position(60, 580);
-    pwd.size(width - 130, 35);
-  pwd.hide();
-  pwd.style('border-style', 'none');
+  screen1Setup();
+}
+
+function screen1Setup() {
+  bg1.hide();
 }
 
 function draw() {
@@ -29,55 +24,14 @@ function draw() {
 }
 
 function screen1() {
-  background(bg);
-
-  if (signUpFadeFlg) {
-    signUpFade -= 10;
-  }
-
-  if (loginRectFadeFlg) {
-    loginY -= 24;
-    loginRectFade -= 10;
-    loginTxtFade -= 20;
-    if(loginRectFade <= 0) {
-      loginRectFadeFlg = false;
-      noStroke();
-      loginTxt = "Log In";
-      loginTxtFade = 255;
-      username.show();
-      pwd.show();
-    }
-  }
-
-  fill(255, 255, 255, loginRectFade);
-  rect(50, loginY, width - 100, 50, 20);
-  fill(0, 0, 0, loginTxtFade);
-  text(loginTxt, 50, loginY + 5, width - 100, 50);
-
-  fill(255, 255, 255, signUpFade);
-  rect(50, 725, width - 100, 50, 20);
-  fill(0, 0, 0, signUpFade);
-  text('Sign Up', 50, 730, width - 100, 50);
-
-  fill(0, 0, 0, 255);
-  if (loginRectFade <= 0) {
-    fill(0, 0, 0);
-    text('<', 20, loginY + 30);
-    fill(255, 255, 255, 255);
-    rect(50, 500, width - 100, 50, 20);
-    rect(50, 575, width - 100, 50, 20);
-    rect(50, 650, width - 100, 50, 20);
-    fill(0, 0, 0);
-  text('Log In', 50, 655, width - 100, 50);
-  }
-}
-
-function touchStarted() {
-  if (screen == 1) {
-    if (mouseX >= 50 && mouseX <= (50 + width - 100) && mouseY >= loginY && mouseY <= loginY + 50) {
-      print("yes");
-      signUpFadeFlg = true;
-      loginRectFadeFlg = true;
-    }
-  }
+  background(220);
+  
+  fill('brown');
+  rect(20, height - 80, width - 40, 50, 25);
+  rect(20, height - 150, width - 40, 50, 25);
+  
+  fill('white');
+  textSize(32);
+  text('Log In', 20, height - 140, width - 40, 50);
+  text('Create An Account', 20, height - 70, width - 40, 50);
 }
