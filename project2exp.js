952bbd0da8db38ac7bab5ad3df6d2bd1a1d8bbdd
palakk07 <90868437+palakk07@users.noreply.github.com>
2021-10-14 03:17:41 -0700

    Update project2exp.js

--- project2exp.js
+++ project2exp.js
@@ -43,7 +43,7 @@ function preload() {
 
 function setup() {
 //   createCanvas(windowWidth, windowHeight);
-  createCanvas(428, 746);
+  createCanvas(428, 927);
   textAlign(CENTER);
   screen1Setup();
   username = createInput();
