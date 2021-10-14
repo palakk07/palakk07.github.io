cb49674101d0ff10bbabf305eab04efb1e67a365
palakk07 <90868437+palakk07@users.noreply.github.com>
2021-10-14 01:12:07 -0700

    Update project2exp.js

--- project2exp.js
+++ project2exp.js
@@ -13,7 +13,7 @@ var tab1, tab2, tab3;
 function preload() {
   bg1 = loadImage('./images/1.png');
   bg2 = loadImage('./images/2.png');
-  bg3 = loadImage('./images/bg3.png');
+  bg3 = loadImage('./images/3.png');
   tab1 = loadImage('./images/home_1.png');
   tab2 = loadImage('./images/schedule_1.png');
   tab3 = loadImage('./images/shop_1.png');
@@ -80,20 +80,6 @@ function screen2() {
 function screen3() {
   background(bg3);
   // image(bg3, 0, 0, width, height);
-  
-  textSize(50);
-  fill('brown');
-  text('Hot Spot Cafe', 20, 100, width - 40, 50);
-  
-  textSize(32);
-  fill('black');
-  text('Till now you have donated 4lbs of used coffee granules', 20, 170, width - 40, 100);
-  
-  fill('brown');
-  rect(20, 300, width - 40, 50, 25);
-  
-  fill('white');
-  text('Schedule Pick Up', 20, 310, width - 40, 50);
   
   image(tab1, width/6 - 25, height - 70, 50, 50);
   image(tab2, width/6 - 25 + width/3, height - 70, 50, 50);
